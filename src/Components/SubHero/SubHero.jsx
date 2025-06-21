a582a079280a09bb0230f8c5b702091653a65151
pramay_usadadiya <info.prem07@gmail.com>
2025-06-21 21:25:45 +0530

    feat: add flex-center-sh class for improved layout and responsiveness

--- src/Components/SubHero/SubHero.jsx
+++ src/Components/SubHero/SubHero.jsx
@@ -11,7 +11,7 @@ function SubHero() {
 
     return (
         <section className='sub-hero gradient-border-black'>
-            <article className="flex-center f1">
+            <article className="flex-center-sh f1">
                 {subHeroContent.map((item, index) => (
                     <article className="child-articales" key={index}>
                         <h3 className='article-title'>{item.title}</h3>
@@ -21,12 +21,12 @@ function SubHero() {
             </article>
 
             <article className="f2">
-                <article className="child-articales flex-center">
+                <article className="child-articales flex-center-sh">
                     <h3 className='article-title'>Years of Experience</h3>
                     <p className='article-num'>10+</p>
                 </article>
 
-                <article className="child-articales know-more flex-center">
+                <article className="child-articales know-more flex-center-sh">
                     <img src={CircleIcon} alt="Circle Icon" className='circle' />
                     <h3 className='know-more-title'>Know More</h3>
                 </article>
